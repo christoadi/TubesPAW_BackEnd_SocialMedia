0a1f42ae19ae64f428e84ff0f00ae52a8b5555b8
christoadi <chrizto.adi456@gmail.com>
2022-12-14 17:38:17 +0700

    Register & Login (Must be fixed)

--- routes/api.php
+++ routes/api.php
@@ -18,12 +18,12 @@ use App\Http\Controllers\EmailVerificationController;
 Route::post('register', 'Api\AuthController@register');
 Route::post('login', 'Api\AuthController@login');
 
-// Verify email
+//Send the link to Verify
 Route::get('/email/verify/{id}/{hash}', [EmailVerificationController::class, '__invoke'])
     ->middleware(['signed', 'throttle:6,1'])
     ->name('verification.verify');
 
-// Resend link to verify email
+//Resend the link to verify
 Route::post('/email/verify/resend', function (Request $request) {
     $request->user()->sendEmailVerificationNotification();
     return back()->with('message', 'Verification link sent!');
@@ -33,6 +33,16 @@ Route::get('/email/verify/success', function () {
     return view('mail');
 });
 
+
+
+
 Route::group(['middleware'=>'auth:api'],function() { //Routes yang bisa dijalankan setelah login
+    //routes untuk user
+    Route::get('user', 'Api\AuthController@index');
+    Route::get('user/{id}','Api\AuthController@show');
+    Route::put('user/{id}','Api\AuthController@update');
+    Route::delete('user/{id}','Api\AuthController@destroy');
+
+    //logout routes
     Route::get('logout','Api\AuthController@logout');
 });
