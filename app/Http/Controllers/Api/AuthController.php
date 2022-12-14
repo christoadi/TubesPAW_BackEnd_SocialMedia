e269c70efa1d08965e161600a28f11673622c386
christoadi <chrizto.adi456@gmail.com>
2022-12-14 17:33:40 +0700

    Register & Login (Must be fixed)

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,87 +3,78 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Mail\EmailVerification;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Validation\Rule;
-// use Validator;
 use Illuminate\Support\Facades\Validator;
-use Illuminate\Support\Facades\Mail;
-use Exception;
-use Carbon\Carbon;
 
 class AuthController extends Controller
 {
+    public function index() {
+        $users = User::all(); //Collecting all user data
+
+        if (count($users) > 0) {
+            return response([
+                'message' => 'Retrieve All Success',
+                'data' => $users
+            ], 200);
+        } //return all user data
+
+        return response([
+            'message' => 'Empty',
+            'data' => null
+        ], 400); //return if empty
+    }
+
     //register
     public function register(Request $request){
         $registrationData = $request->all();
-        $validate = Validator::make($registrationData, [
+        $validate = Validator::make($registrationData, [ //input validation
             'name' => 'required',
             'gender' => 'required',
-            'tanggalLahir' => 'required|date_format:Y-m-d',
-            'email' => 'required|unique:users',
-            'username' => 'required|unique:users',
+            'dateborn' => 'required|date_format:Y-m-d', //date format
+            'email' => 'required|email:rfc,dns|unique:users', //rfc,dns for email input //unique input
+            'username' => 'required|unique:users', //unique input
             'password' => 'required'
-        ]); //input validation
+        ]); 
 
         if ($validate->fails())
-            return response(['message' => $validate->errors()], 400); // return error validasi input
+            return response(['message' => $validate->errors()], 400); //return validation if error in input
 
-        // $temp = $registrationData['password'];
-        $registrationData['password'] = bcrypt($request->password); // enkripsi password
-        $user = User::create($registrationData); // membuat user baru
+        $registrationData['password'] = bcrypt($request->password); //encrypting password
+        $user = User::create($registrationData); //creating user data
 
-        event(new Registered($user));
-
-        // try{
-        //     $date = Carbon::now();
-        //     $detail = [
-        //         'email' => $registrationData['email'],
-        //         'password' => $temp,
-        //         'date' => $date,
-        //     ];
-        //     Mail::to($registrationData['email'])->send(new EmailVerification($detail));
-        //     return response([
-        //         'message' => 'Register Success and email send successfully',
-        //         'user' => $user
-        //     ], 200);
-        // } catch(Exception $e) {
-        //     return response([
-        //             'message' => 'Register Success but cannot send the email',
-        //             'user' => $user
-        //         ], 200);
-        //     }
+        event(new Registered($user)); //Registering user
 
         return response([
             'message' => 'Register Success',
             'user' => $user
-        ], 200); //return data user dalam bentuk json
+        ], 200); //return user data
     }
     
 
-    //Login
+    //Log in
     public function login(Request $request){
         $loginData = $request->all();
-        $validate = Validator::make($loginData, [
+        $validate = Validator::make($loginData, [ //input validation
             'email' => 'required|email:rfc,dns',
             'password' => 'required'
-        ]); // membuat rule validasi input
+        ]);
 
         if ($validate->fails()){
-            return response(['message' => $validate->errors()], 400); // return error validasi input
+            return response(['message' => $validate->errors()], 400); //return validation if error in input
         }    
 
         if (!Auth::attempt($loginData)){
-            return response(['message' => 'Invalid Credentials'], 401); // return error gagal login
+            return response(['message' => 'Invalid Credentials'], 401); //return validation if failed log in
         }    
 
         /** @var \App\Models\User $user **/
         $user = Auth::User();
-        if($user->email_verified_at!=null){
-            $token = $user->createToken('Authentication Token')->accessToken; // generate token
+        if($user->email_verified_at != null){
+            $token = $user->createToken('Authentication Token')->accessToken; //generate token
         }else{
             return response(['message'=>'Email Not verified!'],401);
         }    
@@ -92,41 +83,41 @@ class AuthController extends Controller
             'user' => $user,
             'token_type' => 'Bearer',
             'access_token' => $token
-        ]); // return data user dan token dalam bentuk json
+        ]); //return user data and token in json format
     }
 
-    //show user
+    //show spesific user by id
     public function show($id){
-        $users = User::find($id); //mencari course berdasarkan data id
+        $users = User::find($id); //find user by id
 
         if (!is_null($users)) {
             return response([
                 'message' => 'Retrive User Success',
                 'data' => $users
-            ], 200);
+            ], 200); //return user data by id
         }
 
         return response([
             'message' => 'User Not Found',
             'data' => null
-        ], 404);
+        ], 404); //return if user by id not found 
     }
 
     //Update
     public function update(Request $request, $id){
-        $users = User::find($id);
+        $users = User::find($id); //find user by id
         if(is_null($users)){
             return response([
                 'message'=>'User Not Found',
                 'data'=>null
-            ],400);
+            ],400); //return if user by id not found
         }
 
-        $updateData = $request->all();
-        $validate = Validator::make($updateData, [
-            'name' => 'required|max:60',
+        $updateData = $request->all(); //Validate new data
+        $validate = Validator::make($updateData, [ 
+            'name' => 'required',
             'gender' => 'required',
-            'tanggalLahir' => 'required|date_format:Y-m-d',
+            'dateborn' => 'required|date_format:Y-m-d',
             'username' => ['required',Rule::unique('users')->ignore($users)],
             'password' => ''
         ]); // validasi data
@@ -134,9 +125,10 @@ class AuthController extends Controller
         if ($validate->fails()) {
             return response(['message' => $validate->errors()], 400);
         }
+        //replacing old data with the newest one
         $users->name = $updateData['name'];
         $users->gender = $updateData['gender'];
-        $users->tanggalLahir = $updateData['tanggalLahir'];
+        $users->dateborn = $updateData['dateborn'];
         $users->username = $updateData['username'];
         if ($updateData['password'] != ""){
             $users->password = bcrypt($updateData['password']);
@@ -147,36 +139,36 @@ class AuthController extends Controller
                 'message'=> 'Update User Success',
                 'data'=>$users
             ],200);
-        }
+        } //return if data has been updated
         return response([
             'message'=>'Update User Failed',
             'data'=>null
-        ],400);
+        ],400); //return if update failed
     }
 
-    //delete user
+    //Delete
     public function destroy($id)
     {
-        $users = User::find($id);
+        $users = User::find($id); //find user by id
 
         if(is_null($users)){
             return response([
                 'message'=>'User Not Found',
                 'data'=>null
             ],400);
-        }//return message saat database tidak ditemukan
+        } //return if user by id not found 
 
         if($users->delete()){
             return response([
                 'message'=>'Delete User Success',
                 'data'=>$users
             ],200);
-        }
+        } //return if user has been deleted
 
         return response([
             'message'=>'Delete user Failed',
             'data'=>null
-        ],400);
+        ],400); //return if failed delete user
     }
 
     // public function logout() {
