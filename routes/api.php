2c856f58fc029055ddb97a28ce8fb0ac5ce4286b
christoadi <chrizto.adi456@gmail.com>
2022-12-15 17:39:37 +0700

    Comment CRUD (Must be fixed)

--- routes/api.php
+++ routes/api.php
@@ -33,6 +33,8 @@ Route::get('/email/verify/success', function () {
     return view('mail');
 });
 
+Route::get('feedback/index', 'Api\FeedbackController@index');
+
 
 
 
@@ -42,6 +44,28 @@ Route::group(['middleware'=>'auth:api'],function() { //Routes yang bisa dijalank
     Route::get('user/{id}','Api\AuthController@show');
     Route::put('user/{id}','Api\AuthController@update');
     Route::delete('user/{id}','Api\AuthController@destroy');
+
+    //posting routes
+    Route::get('post/{id}', 'Api\PostController@show');
+    Route::get('post', 'Api\PostController@showAll');
+    Route::get('postfrom/{id}', 'Api\PostController@showUserPosts');
+    Route::delete('post/{id}', 'Api\PostController@destroy');
+    Route::post('post', 'Api\PostController@store');
+    Route::put('post/{id}', 'Api\PostController@update');
+
+
+
+    //comment routes
+    Route::get('comment', 'Api\CommentController@index');
+    Route::get('comment/{id}', 'Api\CommentController@show');
+    Route::get('comment/post/{post_id}', 'Api\CommentController@showInPost');
+    Route::post('comment', 'Api\CommentController@store');
+    Route::post('comment/{post_id}/{user_id}', 'Api\CommentController@storeInPost');
+    Route::put('comment/{id}', 'Api\CommentController@update');
+    Route::delete('comment/{id}', 'Api\CommentController@destroy');
+    Route::delete('comment/post/{idPost}', 'Api\CommentController@destroyPost');
+
+
 
     //logout routes
     Route::get('logout','Api\AuthController@logout');
